a64e5604a3b178b28c375dd9d5d9f3334fee7fc6
b-regan11 <reganbill02@gmail.com>
2024-03-01 15:33:35 -0500

    User Selects File

---
+++ EDD.py
@@ -0,0 +1,12 @@
+# TempQueryName
+
+import os
+import subprocess
+
+# Compile the Java class
+subprocess.run(['javac', 'FileSelection/FileSelection.java'])
+
+# Run the Java class
+subprocess.run(['java', 'FileSelection/FileSelection.java'])
+
+
---
+++ FileSelection/FileSelection.java
@@ -0,0 +1,28 @@
+package FileSelection;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class FileSelection {
+    public static String selectFile() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Excel Files", "xlsx", "csv");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile().getPath();
+        } else {
+            // Return null or an empty string to indicate no file was selected
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example of how to use the selectFile method
+        String selectedFileName = selectFile();
+        if (selectedFileName != null) {
+            System.out.println("You chose to open this file: " + selectedFileName);
+        }
+    }
+}
