b912da1b941c687c1cb26ab059734e498e4b8967
b-regan11 <reganbill02@gmail.com>
2024-03-03 12:58:37 -0500

    Split Scripts to Clean Code

---
+++ BinPacking.py
@@ -0,0 +1,31 @@
+class BinPacking_UrgencyList:
+  def UrgencyList(sorted_data):
+    # Start & End dates for production week
+    start_date = '2023-4-24' # datetime.date(2023,4,24)
+    end_date = '2023-4-30' # datetime.date(2023,4,30)         #both of these should be user input later on
+
+    # Urgency List for Job Priorities
+    UrgencyList = sorted_data[(sorted_data['due_date'] >= start_date) & (sorted_data['due_date'] <= end_date)]
+    UL_Attainable = UrgencyList.query('production_hrs <= 25') # 1
+    UL_Unattainable = UrgencyList.query('production_hrs > 25') # 3
+    UrgencyList = sorted_data[(sorted_data['due_date'] > '2020-1-01') & (sorted_data['due_date'] < start_date)]
+    UL_Overdue_Attainable = UrgencyList.query('production_hrs <= 25') # 2
+    UL_Overdue_Unattainable = UrgencyList.query('production_hrs > 25') # 4
+    OtherList = sorted_data[(sorted_data['due_date'] > end_date) & (sorted_data['due_date'] < '2099-1-01')] # 5
+
+    # Calculate the remainder number of jobs to fill in the other list to total 40 jobs
+    Remainder = 40 - (len(UL_Attainable) + len(UL_Overdue_Attainable) + len(UL_Unattainable) + len(UL_Overdue_Unattainable))
+    OtherList = OtherList.iloc[:Remainder] # Return extra rows to make sure there isn't more than 40 rows of data
+    
+    # Print Urgency Listings for testing purposes
+    print(UL_Attainable)
+    print(UL_Overdue_Attainable)
+    print(UL_Unattainable)
+    print(UL_Overdue_Unattainable)
+    print(OtherList)
+    # For all rows in UL_Attainable, check Frame
+
+# class BinPacking_MachineAvailability:
+#    def __init__(self):
+#         # Initialize an empty dictionary to represent machine schedules
+#         self.machine_schedules = {}
--- EDD.py
+++ EDD.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import pandas as pd
 import datetime
-
+from BinPacking import BinPacking_Functions
 
 # Compile & Run the Java class (File Selection)
 subprocess.run(['javac', 'FileSelection/FileSelection.java'])
@@ -30,30 +30,10 @@ if result.returncode == 0:
         raw_data = pd.read_excel(selected_file_path,"TempQueryName")
         sorted_data = raw_data.query('Completed == "Received" or Completed == "Not Started"') # This is only sorting for status, not date
         sorted_data = sorted_data.sort_values(by=['due_date'])
-        print(sorted_data)
         
-        # Start & End dates for production week
-        start_date = '2023-4-24' # datetime.date(2023,4,24)
-        end_date = '2023-4-30' # datetime.date(2023,4,30)         #both of these should be user input later on
-
-        # Urgency List for Job Priorities
-        UrgencyList = sorted_data[(sorted_data['due_date'] >= start_date) & (sorted_data['due_date'] <= end_date)]
-        UL_Attainable = UrgencyList.query('production_hrs <= 25') # 1
-        UL_Unattainable = UrgencyList.query('production_hrs > 25') # 3
-        UrgencyList = sorted_data[(sorted_data['due_date'] > '2020-1-01') & (sorted_data['due_date'] < start_date)]
-        UL_Overdue_Attainable = UrgencyList.query('production_hrs <= 25') # 2
-        UL_Overdue_Unattainable = UrgencyList.query('production_hrs > 25') # 4
-        OtherList = sorted_data[(sorted_data['due_date'] > end_date) & (sorted_data['due_date'] < '2099-1-01')] # 5
-
-        print(UL_Attainable)
-        print(UL_Overdue_Attainable)
-        print(UL_Unattainable)
-        print(UL_Overdue_Unattainable)
-        print(OtherList)
+        BinPacking_Functions.UrgencyList(sorted_data)
 
         # Tiered Tie Breakers Implementation
 
     else:
         print("No file path returned by the Java program.")
-
-# def BinPacking(data):
